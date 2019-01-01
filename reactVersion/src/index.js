4870d7468e63fd5deeefecf1b10dec2dc9a9db19
Yuxuan Yao <yuxuanyyx@gmail.com>
2018-12-31 19:33:58 -0800

    updated react version for efficiency

--- reactVersion/src/index.js
+++ reactVersion/src/index.js
@@ -2,30 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Box extends React.Component {
-    render() {
-        return (
-            <div id="box1">
-                <h1>Parallax</h1>
-            </div>
-        )
-    }
+function Box(props) {
+    return (
+        <div className={props.cName}>
+            <h1>Parallax</h1>
+        </div>
+    )
+
 }
 
-
 class TopLevel extends React.Component {
+
+    renderBox(name) {
+        return (
+            <Box
+                cName={name}
+            />
+        );
+    }
+
     render() {
+        let i = 0;
         return (
             <div>
-                <div id="box1">
-                    <h1>Parallax</h1>
-                </div>
-                <div id="box2">
-                    <h1>Parallax</h1>
-                </div>
-                <div id="box3">
-                    <h1>Parallax</h1>
-                </div>
+                {this.renderBox('box box' + (++i))}
+                {this.renderBox('box box' + (++i))}
+                {this.renderBox('box box' + (++i))}
             </div>
         );
     }
