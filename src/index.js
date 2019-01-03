f59524bb4581e7d8192c274a3b0f40a7e82a7221
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-01-02 22:22:07 -0800

    made responsive

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ class Box extends React.Component {
 
     // functions to blur background on hover
     blurBg = () => {
+        // note the space before back-blur
         this.setState({ blur: " back-blur" });
     }
 
@@ -32,13 +33,17 @@ class Box extends React.Component {
     }
     renderBtns() {
         var tBtnArr = [];
+        // loop through all buttons for this section
         for (var i = 0; i < this.props.tBtn.length; ++i) {
+            // photos button for special effects
             if (this.props.tBtn[i][0] == "Photos") {
-                if (this.props.tBtn[i][1] != "#") {
+                // if no link, don't make it clickable
+                if (this.props.tBtn[i][1] == "#") {
                     tBtnArr.push(
                         <div className="terminalBtn" onMouseEnter={this.unblurBg} onMouseLeave={this.blurBg}>{this.props.tBtn[i][0]}</div>
                     );
                 }
+                // makes it clickable
                 else {
                     tBtnArr.push(
                         <a href={this.props.tBtn[i][1]} target="_blank">
@@ -47,6 +52,7 @@ class Box extends React.Component {
                     );
                 }
             }
+            // other buttons
             else {
                 tBtnArr.push(
                     <a href={this.props.tBtn[i][1]} target="_blank">
@@ -55,7 +61,6 @@ class Box extends React.Component {
                 );
             }
         }
-
         return (
             <div className="btnContainer">
                 {tBtnArr}
@@ -74,11 +79,11 @@ class Box extends React.Component {
                 <div className="text" >
                     <div className="scroll-container" data-aos='zoom-in-right'>
                         <div className="bar">
-                            <div className="red">
+                            <div className="tCircles red">
                             </div>
-                            <div className="yellow">
+                            <div className="tCircles yellow">
                             </div>
-                            <div className="green">
+                            <div className="tCircles green">
                             </div>
                         </div>
                         <div className={"screen"}>
