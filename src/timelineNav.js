a2d106d59c6c01042c73d0bcf5931110c944081e
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-01-03 23:21:27 -0800

    made sidebar kinda responsive

--- src/timelineNav.js
+++ src/timelineNav.js
@@ -6,28 +6,46 @@ class TlNav extends React.Component {
 
     render() {
         var tlNavItems = [];
+        // timeline NavBar Items (titles)
+        var tLineCircArr = [];
 
         for (let i = 0; i < titles.length; ++i) {
             tlNavItems.push(
                 // need to add href
                 <div className={"tlNavItems "}>{titles[i]}</div>
             );
+            tLineCircArr.push(
+                <div className={"tLineCircle"}></div>
+            );
         }
+
+
 
         return (
             <div className="tlNav">
+
                 {/* Icon for Opening and Closing NavBar */}
                 <div className={this.props.tlNav + "Container"}>
                     <div className={"tlNavIcon" + this.props.tlNav} onClick={this.props.navClickHandler}></div>
                 </div>
+
                 {/* to fix background transition */}
                 <div className={this.props.tlNav + "BgFix"}></div>
                 {/* NavBar Contents aka Titles  */}
                 <div className={this.props.tlNav + "Content"}>
+                    {/* the actual line */}
+                    <div className={"tLine" + ((this.props.tlNav === " tlNavOpen") ? "Open" : "Closed")}>
+                        {/* the circles in the line */}
+                        <div className={"tLineCircContainer"}>
+                            {tLineCircArr}
+                        </div>
+                    </div>
                     <div className={"tlNavListContainer " + ((this.props.tlNav === " tlNavOpen") ? " listDisplay" : " listNoDisplay")}>
                         {tlNavItems}
                     </div>
+
                 </div>
+
             </div >
 
         );
